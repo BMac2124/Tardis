4d9f21fd65c48d65ca3f1447f5165f4ca5e52a55
BMac2124 <87466319+BMac2124@users.noreply.github.com>
2021-08-30 20:16:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import DivOne from './DivOne';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    tardis: {
+      name: 'Time and Relative Dimension in Space',
+      caps: false,
+    }
+  }
+  
+  changeIt = (text) =>{
+    if (this.state.tardis.caps) {
+      this.setState({
+        tardis: {
+          name: text.toLowerCase(),
+          caps: false
+        }
+      })
+    } else {
+      this.setState({
+        tardis: {
+          name: text.toUpperCase(),
+          caps: true
+        }
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <DivOne DivOne={this.state.tardis.name} />
+        
+      </div>
+    )
+  }
 }
-
-export default App;
